8b86d6e23a76ae4e391248b44097936800a352a5
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-05-11 20:00:51 +0700

    pembuatan class dokter

--- Asosisasi.cpp
+++ Asosisasi.cpp
@@ -27,3 +27,25 @@ public:
 
 };
 
+//pembuatan class dokter
+class dokter {
+public:
+    //deklarasi member variabel dari class dokter
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    //pembuatan constructor dan destructor dari class dokter
+    dokter(string pNama) :nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+
+    ~dokter() {
+        cout << "Dokter \"" << nama << "\" tidak ada\n";
+    }
+
+    //deklarasi prosedur tambahPasien() dan cetakPasien()
+    void tambahPasien(pasien*);
+    void cetakPasien();
+
+};
+
