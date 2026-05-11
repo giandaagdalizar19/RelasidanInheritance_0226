f528d9572cac5b753399a292a48e2ecfc0a45562
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-05-11 19:58:23 +0700

    mulai dan class

--- jantung.h
+++ jantung.h
@@ -1,10 +1,9 @@
-#ifndef JANTUNG_H
-#define JANTUNG_H
+#include <iostream>
+#include <string>
+using namespace std;
 
-//membuat class jantung di file header jantung.h
 class Jantung {
 public:
-    //membuat constructor dan destructor dari class jantung
     Jantung() {
         cout << "Jantung Ditambah\n";
     }
@@ -13,21 +12,3 @@ public:
     }
 };
 
-#endif // !JANTUNG_H
-komposisi
-#include <iostream>
-#include <string>
-using namespace std;
-
-//menyertakan file header jantung.h dan manusia.h
-#include "jantung.h"
-#include "manusia.h"
-
-int main()
-{
-    //deklarasi dan pemberian nilai object pointer varManusia
-    Manusia* varManusia = new Manusia("jono");
-    //mendelete pointer varManusia dari memori
-    delete varManusia;
-    return 0;
-}
