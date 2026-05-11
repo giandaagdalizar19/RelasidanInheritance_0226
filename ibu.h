95ab78563b0c4ffd2919c92f91fe583a9bd43d7c
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-05-11 18:42:47 +0700

    mulai

--- ibu.h
+++ ibu.h
@@ -7,33 +7,3 @@
 #include "anak.h"
 using namespace std;
 
-class anak;
-class ibu {
-public:
-    string nama;
-    vector<anak*> DaftarAnak;
-
-    ibu(string pNama) : nama(pNama) {
-        cout << "Ibu " << nama << " ada" << endl;
-    };
-
-    ~ibu() {
-        cout << "Ibu " << nama << " tidak ada" << endl;
-    };
-
-    void tambahAnak(anak* pAnak);
-    void cetakAnak();
-};
-
-void ibu::tambahAnak(anak* pAnak) {
-    DaftarAnak.push_back(pAnak);
-}
-
-void ibu::cetakAnak() {
-    cout << "Daftar anak dari Ibu " << nama << ":" << endl;
-    for (size_t i = 0; i < DaftarAnak.size(); i++) {
-        cout << "- " << DaftarAnak[i]->nama << endl;
-    }
-}
-
-#endif
