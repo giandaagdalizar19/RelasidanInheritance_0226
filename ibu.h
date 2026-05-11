bfea48fefee631e3a98e7ea5d785409bcff55795
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-05-11 18:43:44 +0700

    deklarasi

--- ibu.h
+++ ibu.h
@@ -7,3 +7,21 @@
 #include "anak.h"
 using namespace std;
 
+class anak;
+class ibu {
+public:
+    string nama;
+    vector<anak*> DaftarAnak;
+
+    ibu(string pNama) : nama(pNama) {
+        cout << "Ibu " << nama << " ada" << endl;
+    };
+
+    ~ibu() {
+        cout << "Ibu " << nama << " tidak ada" << endl;
+    };
+
+    void tambahAnak(anak* pAnak);
+    void cetakAnak();
+};
+
